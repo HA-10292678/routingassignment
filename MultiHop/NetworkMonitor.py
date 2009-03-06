778d6124b23a0eb274102046a00ffd37f192187d
OliverWS <OliverWS@b8e39a54-04ef-11de-a320-db7d8e7a638f>
2009-03-06 18:20:54 +0000

    added preliminary python script

---
+++ MultiHop/NetworkMonitor.py
@@ -0,0 +1,27 @@
+import sys,os
+sys.path.append(os.path.join(os.environ["TOSROOT"], "support/sdkpython"))
+from tinyos.message import MoteIF
+import MultihopMsg
+
+class NetworkMonitor:
+    def __init__(self):
+        self.mif = MoteIF.MoteIF()
+        self.mif.addSource("sf@localhost:9002")
+        self.mif.addListener(self, MultihopMsg.MultihopMsg)
+
+    def receive(self, src, msg):
+        if msg.get_amType() == MultihopMsg.AM_TYPE:
+	    print "source: " + msg.get_source() + " ";
+	    print "seqnum: " + msg.get_seqnum() + " ";
+	    print "treedepth: " + msg.get_treedepth() + " ";
+	    print "data: " + msg.get_data() + " ";
+ 	    print "\n";    	
+    def sendMsg(self, addr, amType, amGroup, msg):
+        self.mif.sendMsg(self.source, add, amType, amGroup, msg)
+
+listener = NetworkMonitor()
+msg = MultihopMsg.MultihopMsg()
+
+while True:
+    print "Waiting..."
+    time.sleep(1)
